4f8b1cc0a5728212c6ed1aabd08f71a70870846c
MarcoRustemi <Marco.Rustemi@stud.fh-campuswien.ac.at>
2023-01-09 23:34:35 +0100

    Update

--- src/main/java/at/ac/campuswien/weatherapp/WeatherController.java
+++ src/main/java/at/ac/campuswien/weatherapp/WeatherController.java
@@ -1,8 +1,24 @@
 package at.ac.campuswien.weatherapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.net.MalformedURLException;
 
 public class WeatherController {
 
+    @FXML
+    private TextField city;
+
+    @FXML
+    private Text weatherOutput;
+
+    private final String cityAPI = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid={d956a4bba747963b38b35352359883a0}";
+
+    @FXML
+    void getWeatherData(ActionEvent event) throws MalformedURLException {
+    }
 }
