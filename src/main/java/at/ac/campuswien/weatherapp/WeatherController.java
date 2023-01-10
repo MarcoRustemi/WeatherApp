7c03d8c6eb113a30f2e53c9b2b81be6531d68808
lukatr1 <luka.trifunovic@stud.fh-campuswien.ac.at>
2023-01-10 13:52:31 +0100

    changed style

--- src/main/java/at/ac/campuswien/weatherapp/WeatherController.java
+++ src/main/java/at/ac/campuswien/weatherapp/WeatherController.java
@@ -1,6 +1,55 @@
 package at.ac.campuswien.weatherapp;
 
-import javafx.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class WeatherController {
+
+    @FXML
+    private TextField stateField;
+
+    @FXML
+    private void searchWeather() {
+        String state = stateField.getText();
+        String apiKey = "644660f15c460cd498ab3932a80a5b9b";
+
+        String url = "https://api.weather.com/v3/wx/forecast/daily/5day?format=json&units=m&language=en-US&state=" + state + "&apiKey=" + apiKey;
+
+        try {
+            String jsonResponse = makeAPICall(url);
+            // Parse JSON response to retrieve weather information
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String makeAPICall(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        return content.toString();
+    }
+}
+
+
+
+
+/*import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -20,4 +69,4 @@ public class WeatherController {
     @FXML
     void getWeatherData(ActionEvent event) throws MalformedURLException {
     }
-}
+}*/
